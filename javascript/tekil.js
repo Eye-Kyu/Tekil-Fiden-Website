721f56fb58e394df37dec90b6ca222c25cbfc625
Pancrass Njoroge <119348561+Eye-Kyu@users.noreply.github.com>
2024-01-14 11:49:16 +0300

    Add files via upload

--- javascript/tekil.js
+++ javascript/tekil.js
@@ -1,30 +1,14 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-//Toggle Nav
-    burger.addEventListener('click' , () => {
-        nav.classList.toggle('nav-active');
-    });
-//animate Links
-    navLinks.forEach((link, index) => {
-     if (link.style.animation) {
-        link.style.animation = '';
-     } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards  ${index / 7 + 0.5}s`;
-        
-     }
-     //burger animation
-     burger.classList.toggle('toggle'); 
-        
-    });
-}
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+    if(entry.isIntersecting) {
+        entry.target.classList.add('show');
 
-navSlide();
-  //explore portfolio
-  $('.counter').counterUp({
-    delay: 10,
-    time: 2000
-  });
-  $('.counter').addClass('animated fadeInDownBig');
-  $('h3').addClass('animated fadeIn');
+    }else{
+        entry.target.classList.remove('show');
+    }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
