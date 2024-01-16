aad7b2b394848ddc65653ff1e1da306c70c527c3
Pancrass Njoroge <119348561+Eye-Kyu@users.noreply.github.com>
2024-01-16 14:57:44 +0300

    Add files via upload

--- javascript/type.js
+++ javascript/type.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["Security", "Reliability", "Scale", "Range"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 200; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
