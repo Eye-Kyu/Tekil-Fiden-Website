15fa91099925dee12603c1294efd66cd2a7392ac
Pancrass Njoroge <wankenobi29@gmail.com>
2024-05-29 19:43:47 +0300

    git commit -m "gsap update"

--- javascript/gsap.js
+++ javascript/gsap.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const timeline = gsap.timeline({paused: true});
 
     timeline.to(".overlay", {
-        duration: 1.5,
+        //duration: 1.5,
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
         ease: "power4.inOut"
     });
@@ -135,4 +135,99 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         isOpen = !isOpen;
     })
+})
+
+gsap.set('.mbisha', {
+    y: 32,
+    opacity : 0
+})
+gsap.set('.SMS', {
+    y: 32,
+    opacity : 0
+})
+gsap.set('.weoffer', {
+    y: -12,
+    opacity : 0
+})
+
+gsap.set('.def', {
+    opacity : 0,
+})
+
+const tl2 = gsap.timeline({delay:0});
+
+
+tl2.to(".mbisha", {
+    top: 0,
+    opacity : 1,
+    duration: 2,
+    ease: "power1.inOut"
+});
+
+tl2.to(".weoffer", {
+    opacity: 1,
+    duration: 1,
+    y: 22,
+    ease: "power2.inOut"
+}, "-=1");
+   tl2.to(".Clients", {
+   backgroundColor: '#a5a5a5',
+    scrollTrigger: {
+        trigger: '.Clients',
+        start: 'top 80%',
+        end: "+=600",
+        backgroundColor: '#a5a5a5',
+        scrub: 1,
+        duration: 1
+    }
+ });
+
+//contact page 
+gsap.to(".SMS", {
+    top: 0,
+    opacity : 1,
+    duration: 2,
+    ease: "power1.inOut"
+});
+
+
+ //sticky nav on Scroll
+ gsap.set('.nav-bg', {
+    backgroundColor: '#000',
+    opacity: 0
+ })
+ gsap.set('b2top', {
+    yPercent: -100
+ })
+ const tl1 = gsap.timeline({
+    scrollTrigger: {
+        start: 'top+=200',
+        end: '+=1',
+        toggleActions: 'play none none reverse',
+        scrub: 2
+    }
+ })
+
+ tl1.to('.nav-bg', {
+    opacity: 1
+ })
+
+
+ //stagger scrolling (pg 2)
+ const tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.mboka',
+        scrub: 1,
+        start: 'top 80%', 
+        end: "+=800",
+        duration: 3,
+        toggleActions: 'play none none reverse',
+        ease: 'power4.inOut'
+    }
+})
+.to ('.def', {
+    stagger: .2,
+    y: -100,
+    opacity: 1,
+    scrub: true
 })
--- javascript/nav.js
+++ javascript/nav.js
@@ -1,3 +1,4 @@
+//Preloader
 const preloader = document.querySelector("[data-preload]");
 
 window.addEventListener("load" , function()  {
@@ -5,17 +6,6 @@ window.addEventListener("load" , function()  {
   document.body.classList.add("loaded");
 });
   
-/*
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
-}
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
-}
-*/
-
 
 
 
