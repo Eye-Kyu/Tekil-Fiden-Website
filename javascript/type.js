083f05f732debed511024d26d387635353f3f96f
Pancrass Njoroge <wankenobi29@gmail.com>
2024-06-04 12:31:21 +0300

    git commit -m "updated index"

--- javascript/type.js
+++ javascript/type.js
@@ -39,6 +39,3 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
-
-
-
